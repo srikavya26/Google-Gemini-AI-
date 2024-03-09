88f7123683a5a79fcbc7ce5220c39cfb5008eff0
Srikavya Vasala <95865936+srikavya26@users.noreply.github.com>
2024-03-09 20:03:54 +0530

    Add files via upload

---
+++ Gen AI/src/App.js
@@ -0,0 +1,12 @@
+const App = () => {
+  return (
+    <div className="app">
+      <section className="search-section">
+        <p>What do you want to know? <button className="surprise">Suprise me</button></p>
+
+      </section>
+    </div>
+  )
+}
+
+export default App;
